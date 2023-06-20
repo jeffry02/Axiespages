fa8c55375cbebb34a15f85e2a093c877ab2256ae
Allan Martinez <jeffrymart02@gmail.com>
2023-06-19 23:58:27 -0600

    update the email massage

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,32 +2,63 @@
 <html>
 <head>
   <title>Welcome to Axies</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
 </head>
 <style>
   * {
-    background: #374151;
+    background: #403f4c;
+    font-family: 'Tilt Neon', cursive, sans-serif;
   }
+
+  body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 80px;
+  }
+
+  .main {
+    width: 1700px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 25px;
+    align-items: center;
+  }
+
   h1 {
-    font-size: 18px;
+    font-size: 22px;
     color: white;
     font-weight: 500;
   }
+
   .text {
     font-weight: 400;
-    font-size: 16px;
+    font-size: 18px;
     color: white;
     display: flex;
+    flex-direction: column;
+    row-gap: 10px;
+    text-align: justify;
   }
-  .link{
-    color:blue;
+
+  #link{
+    color: #00bbf9;
+  }
+
+  #footer{
+    font-size: 22px;
   }
 </style>
 <body>
-<h1>Thank you very much {{$name}} for subscribing to our website!</h1>
+<div class="main">
+  <h1>Thank you very much {{$name}} for subscribing to our website!</h1>
 
-<div class="text">
-  <p>AXIES is a sales and purchase site where the most important part of this is YOU {{$name}}, We appreciate that you have chosen our shopping site, I hope you enjoy it and that you help us to continue growing. Share the belong link <span id="link">"http://Axies.com.ni"</span> with your friends and family, we will thank you very much, In the same way, we share you our social medias <span id="link">"https://www.facebook.com/Axies"</span> <span id="link">"https://www.instagram.com/Axieshop/"</span> So you will be aware of seasonal promotions and discounts.</p>
-    <span>and the most important thing that you enjoy your purchases be welcome!</span>
+  <div class="text">
+    <p>AXIES is a sales and purchase site where the most important part of this is YOU {{$name}}. We appreciate that you have chosen our shopping site, I hope you enjoy it and that you help us to continue growing. Share the belong link <span id="link">"http://Axies.com.ni"</span> with your friends and family we will thank you very much, In the same way, we share you our social medias <span id="link">"https://www.facebook.com/Axies"</span> or <span id="link">"https://www.instagram.com/Axieshop/"</span> So you will be aware of seasonal promotions and discounts.</p>
+    <span id="footer">And the most important thing that you enjoy your purchases be welcome!</span>
+  </div>
 </div>
 </body>
 </html>
